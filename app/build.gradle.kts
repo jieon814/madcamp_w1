6a36a3c9a63295525ebcdeb0972dedf550b532fd
jieon814 <maria6645@kaist.ac.kr>
2024-12-28 16:22:33 +0900

    nothing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.15.1")
     implementation("androidx.viewpager2:viewpager2:1.1.0-beta01")
     implementation("com.google.android.material:material:1.12.0")
+    // 리싸이클 뷰를 위한 dependency 추가
+    implementation("com.android.support:recyclerview-v7:28.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,4 +51,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
