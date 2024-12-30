04ebd6a5f0600b593693a630c4c3778153081a8f
jieon814 <maria6645@kaist.ac.kr>
2024-12-30 22:27:49 +0900

    after merged

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
 
     implementation("androidx.viewpager2:viewpager2:1.1.0-beta01")
     implementation("com.google.android.material:material:1.12.0")
+    // 리싸이클 뷰를 위한 dependency 추가
+    implementation("com.android.support:recyclerview-v7:28.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -54,4 +57,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
